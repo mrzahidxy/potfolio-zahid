5f371db7dcb84a24e90c459d14f02616f9a84096
Zahid Hasan <mrzahidxy.com>
2024-10-14 11:21:34 +0600

    ui modification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,11 +17,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="bg-slate-100 lg:h-screen dark:bg-gray-800 dark:text-white py-1">
-          <Navbar />
-          <div className="container  lg:h-[calc(100vh-5rem)]">{children}</div>
-        </div>
+      <body
+        className={`${inter.className} h-screen  dark:bg-gray-800 dark:text-white`}
+      >
+        <Navbar />
+        <div className="container lg:h-[calc(100vh-5rem)]">{children}</div>
       </body>
     </html>
   );
